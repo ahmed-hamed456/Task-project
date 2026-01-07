fdedbd300773381da8a51c036fcd08f69f93eaf8
ahmed-hamed456 <ahmedhamed742002@gmail.com>
2026-01-07 11:02:49 +0200

    update assembly informational version and cache files for OrderManagement components

--- backend/src/OrderManagement.API/obj/Debug/net10.0/OrderManagement.API.AssemblyInfo.cs
+++ backend/src/OrderManagement.API/obj/Debug/net10.0/OrderManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65587d2ae1223c8fe50512aec3f165d4a3e18780")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
